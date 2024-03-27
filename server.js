2f2ee00115413367c33f3db9097159655677147e
Hamza Iftikhar <hamza.iftikhar@allente.tv>
2024-03-27 19:27:16 +0500

    First Push

--- server.js
+++ server.js
@@ -6,7 +6,27 @@ import feedRoutes from "./routes/feed_routes.js";
 env.config();
 import db from "./config/db.js";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: "*",
+
+    // origin: function (origin, callback) {
+    //   // allow requests with no origin
+    //   // (like mobile apps or curl requests)
+    //   if (!origin) return callback(null, true);
+    //   if (allowedOrigins.indexOf(origin) === -1) {
+    //     const msg =
+    //       "The CORS policy for this site does not " +
+    //       "allow access from the specified Origin.";
+    //     return callback(new Error(msg), false);
+    //   }
+    //   return callback(null, true);
+    // },
+    // https://strange-dirndl-deer.cyclic.app
+  })
+);
+
 const PORT = process.env.PORT;
 app.use(express.json());
 
